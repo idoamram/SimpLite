a5443e28bbef43ee83dc731471207efc88dc4c65
idoamram <ido.movieditor@gmail.com>
2016-12-04 13:26:41 +0200

    bintray integrations cleanups

--- simplite-orm/src/main/java/com/simplite/orm/DatabaseHelper.java
+++ simplite-orm/src/main/java/com/simplite/orm/DatabaseHelper.java
@@ -35,60 +35,31 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         return databaseInstance;
     }
 
-    public static void closeIfExist() {
-        if (helperInstance != null)
-            helperInstance.close();
-    }
-
     @Override
     public void onCreate(SQLiteDatabase db) {
         if (config != null) config.beforeOnCreate(context);
         try {
-//            db.execSQL(new MaterialItem(context).getCreateTableCommand());
-//            db.execSQL(new Cycle(context).getCreateTableCommand());
-//            db.execSQL(new Course(context).getCreateTableCommand());
-//            db.execSQL(new Rating(context).getCreateTableCommand());
-//            db.execSQL(new MalshabItem(context).getCreateTableCommand());
-//            db.execSQL(new FaqItem(context).getCreateTableCommand());
             ArrayList<String> createTableCommands = ManifestProvider.getCreateTableCommands(context);
             for (String command : createTableCommands) {
                 db.execSQL(command);
             }
-//            initializePreferences();
-            Log.i("SimpLite", "onCreate: All tables created successfully");
+            Log.i("SimpLite", "onCreate: All tables created successfully (" +
+                    createTableCommands.size() + " tables)");
         } catch (Exception e) {
             e.printStackTrace();
         }
         if (config != null) config.afterOnCreate(context);
     }
 
-//    private void initializePreferences() {
-//        try {
-//            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(context);
-//            sharedPreferences.edit().putBoolean(Constants.PREF_IS_FIRST_RUN, true).apply();
-//            sharedPreferences.edit().putBoolean(Constants.PREF_IS_UPDATE_SERVICE_SUCCESS_ONCE, false).apply();
-//            sharedPreferences.edit().putLong(Constants.PREF_LAST_UPDATE_TIMESTAMP, 0).apply();
-//            sharedPreferences.edit().putInt(Constants.PREF_DATABASE_VERSION, ManifestProvider.getDatabaseVersion(context)).apply();
-//        } catch (Exception e) {
-//            e.printStackTrace();
-//        }
-//    }
-
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         if (config != null) config.beforeOnUpgrade(context);
-//        db.execSQL(MaterialItem.getDropTableIfExistCommand(MaterialItem.class));
-//        db.execSQL(Course.getDropTableIfExistCommand(Course.class));
-//        db.execSQL(Cycle.getDropTableIfExistCommand(Cycle.class));
-//        db.execSQL(Rating.getDropTableIfExistCommand(Rating.class));
-//        db.execSQL(MalshabItem.getDropTableIfExistCommand(MalshabItem.class));
-//        db.execSQL(FaqItem.getDropTableIfExistCommand(FaqItem.class));
         ArrayList<String> dropTableCommands = ManifestProvider.getDropTableCommands(context);
         for (String command : dropTableCommands) {
             db.execSQL(command);
         }
         Log.i("SimpLite", "onUpgrade: Upgrade from version " + oldVersion + " to version " +
-                newVersion + ".\nAll tables dropped successfully");
+                newVersion + ".\nAll tables dropped successfully (" + dropTableCommands.size() + " tables)");
         onCreate(db);
         if (config != null) config.afterOnUpgrade(context);
     }
